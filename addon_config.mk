8de87581541a3c51a6811bc93b9b80f6e59d1a64
Gromit Park <bemore.one@gmail.com>
2025-04-16 20:59:51 +0900

    Add addon_config.mk

---
+++ addon_config.mk
@@ -0,0 +1,11 @@
+# Addon config file
+meta:
+	ADDON_NAME = ofxSpectralSaliency
+	ADDON_DESCRIPTION = Spectral Residual Saliency Map addon for OpenFrameworks
+	ADDON_AUTHOR = bemoregt
+	ADDON_TAGS = "computer vision" "saliency" "spectral residual" "image processing"
+	ADDON_URL = http://github.com/bemoregt/ofxSpectralSaliency
+
+common:
+	# dependencies with other addons
+	ADDON_DEPENDENCIES = ofxOpenCv ofxCv ofxGui
